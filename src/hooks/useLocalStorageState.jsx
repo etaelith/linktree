c03db175508b7c1affcf80255461b141d59e945c
etaeleith <sabas.ll.w@gmail.com>
2023-05-05 21:35:16 -0300

    0.0.24 - LocalStorage

--- src/hooks/useLocalStorageState.jsx
+++ src/hooks/useLocalStorageState.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-
+const isClientSide = () => typeof window !== "undefined";
 const useLocalStorageState = (key, defaultValue) => {
   const [state, setState] = useState(() => {
+    if (!isClientSide()) return defaultValue;
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : defaultValue || [];
   });
   useEffect(() => {
+    if (!isClientSide()) return;
     localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
   return [state, setState];
